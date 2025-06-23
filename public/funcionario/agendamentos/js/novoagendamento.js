a4188b443a26344e41d1a979ba8b182e6ab397bf
Milena Borges <mileninhaborghes@hotmail.com>
2025-06-23 12:08:06 -0300

    Update novoagendamento.js

--- public/funcionario/agendamentos/js/novoagendamento.js
+++ public/funcionario/agendamentos/js/novoagendamento.js
@@ -367,4 +367,12 @@ onAuthStateChanged(auth, (user) => {
   dataInput.value = hojeMin;
 
   popularHorasDisponiveis();
+  const menuToggle = document.getElementById('menuToggle');
+  const navLinks = document.getElementById('navLinks');
+
+  if (menuToggle && navLinks) {
+    menuToggle.addEventListener('click', () => {
+      navLinks.classList.toggle('show');
+    });
+  }
 });
